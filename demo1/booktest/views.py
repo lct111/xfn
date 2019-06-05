6895b20a36aa054a592c717c4c92b31ea7379ed6
xfn <758693979@qq.com>
2019-06-05 15:15:55 +0800

    交互

--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(req):
+    return HttpResponse('这是首页')
+
+def list(req):
+    return HttpResponse('这是列表页')
+
+def detail(req,id):
+    return HttpResponse('这是详情页%s'%(id,))
