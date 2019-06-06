4f07923d0034ddc8733814c8ebf2d34587be548a
xfn <758693979@qq.com>
2019-06-06 09:08:05 +0800

    回到之前

--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
 class BookInfo(models.Model):
+
     title=models.CharField(max_length=20)
-    pub_date=models.DateTimeField()
+    pub_date=models.DateField()
 
 class HeroInfo(models.Model):
+
     name=models.CharField(max_length=20)
     gender=models.BooleanField(default=True)
-    contcnt=models.CharField(max_length=100)
-    #book外键
+    content=models.CharField(max_length=100)
+    #book 作为外键关联到bookinfo表
     book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
--- demo1/booktest/views.py
+++ demo1/booktest/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import BookInfo,HeroInfo
+from django.template import loader
 
 # Create your views here.
+
+def index(req):
+    # return HttpResponse('这是首页')
+    temp=loader.get_template('booktest/index.html')
+    res=temp.render({})
+    return HttpResponse(res)
+
+def list(req):
+    books=BookInfo.objects.all()
+
+    # return HttpResponse('这是列表页')
+    res=loader.get_template('booktest/list.html').render({'books':books})
+    return HttpResponse(res)
+
+def detail(req,id):
+    book=BookInfo.objects.get(pk=id)
+    # return HttpResponse('这是详情页%s'%(id,))
+    res=loader.get_template('booktest/detail.html').render({'book':book})
+    return HttpResponse(res)
