d282a2971da1083c83127dca89bb0141ab8b51eb
xfn <758693979@qq.com>
2019-06-06 11:42:38 +0800

    解除硬包

--- demo1/demo1/urls.py
+++ demo1/demo1/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url('',include('booktest.urls')),
+    url('',include('booktest.urls',namespace='booktest')),
 ]
