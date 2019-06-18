90ea1df6cead8750c27337bca8b48539718c9e7d
xfn <758693979@qq.com>
2019-06-18 21:11:29 +0800

    STATIC_ROOT

--- demo3/demo3/settings.py
+++ demo3/demo3/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 
-STATIC_ROOT = '/home/sss/xfn/xfn/staticroot'
+STATIC_ROOT = '/home/sss/xfn/xfn/demo3/staticroot'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/media')
 
